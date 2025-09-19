338b58c18cb3a7992f48ae048ce91738102a3f7e
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-09-19 09:47:07 -0300

    feat: add example of correct and incorrect use of apostrophes in strings

---
+++ cap02/10.apostrophe.py
@@ -0,0 +1,6 @@
+# correct use of string
+apostrophe = "One of Python's strengths is its diverse community"
+print(apostrophe)
+
+# incorrect use of string
+# apostrophe = 'One of Python's strengths is its diverse community'
