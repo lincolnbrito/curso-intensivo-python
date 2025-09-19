da64f2487bcadd243285f370ac33a4bb7d07a0e7
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-09-19 16:25:27 -0300

    feat: remove item from list using remove method

---
+++ chap03/07.remove_list_item.py
@@ -0,0 +1,8 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+
+print(motorcycles)
+
+# remove an item (only the first instance) based on its value
+motorcycles.remove('yamaha')
+
+print(motorcycles)
