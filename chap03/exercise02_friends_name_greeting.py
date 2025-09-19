2561d973efd60f16d49d9d4a1c1b01fee94c2e69
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-09-19 15:18:07 -0300

    feat: exercise using lists

---
+++ chap03/exercise02_friends_name_greeting.py
@@ -0,0 +1,6 @@
+friends = ['John', 'Paul', 'George', 'Ringo']
+greeting = "Hi, "
+print(greeting + friends[0])
+print(greeting + friends[1])
+print(greeting + friends[2])
+print(greeting + friends[3])
