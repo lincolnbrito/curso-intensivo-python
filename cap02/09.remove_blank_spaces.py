22c9830d74220b12ffa52e8c4bfc364aea900072
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-09-19 09:40:21 -0300

    feat: remova blank spaces

---
+++ cap02/09.remove_blank_spaces.py
@@ -0,0 +1,11 @@
+favorite_language = " Python "
+print(favorite_language)
+
+# remove blank space at right position
+print(favorite_language.rstrip())
+
+# remove blank space at left position
+print(favorite_language.lstrip())
+
+# remove blank space at start and final position
+print(favorite_language.strip())
