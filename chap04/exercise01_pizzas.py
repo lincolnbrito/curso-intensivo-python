ce4dd8503c7629752c9b6f75a67a01a05ee7bee7
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-09-22 12:42:46 -0300

    feat: exercise with for loops on lists and final instruction

---
+++ chap04/exercise01_pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ["mozzarella", "pepperoni", "bacon"]
+
+for pizza in pizzas:
+    print("I like " + pizza + " pizza!")
+
+print("\nI really like pizza!")
---
+++ chap04/exercise02_animals.py
@@ -0,0 +1,6 @@
+animals = ["dog", "cat", "bird"]
+
+for animal in animals:
+    print(animal + " would be a good pet")
+
+print("Any of these animals would be a good pet!")
