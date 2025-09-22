e260197e22ec083f999fc6bfd09f9dda6ca109e5
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-09-22 11:18:07 -0300

    feat: sort and reverse a list

---
+++ chap03/exercise03_list_size.py
@@ -0,0 +1,13 @@
+locations = ['usa', 'greece', 'italy', 'japan', 'china']
+
+print(locations)
+print(sorted(locations))
+print(locations)
+
+print(sorted(locations, reverse=True))
+print(locations)
+
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
