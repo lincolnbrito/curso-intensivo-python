d8dc5db7f0e7eefc184c19b48e5600857a2cff21
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-09-23 17:02:18 -0300

    feat: if instruction in lists

---
+++ chap05/08.if_lists.py
@@ -0,0 +1,6 @@
+requested_toppings = ["mushrooms", "onions", "green peppers" "extra cheese"]
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping)
+
+print("\nFinishing making your pizza!")
