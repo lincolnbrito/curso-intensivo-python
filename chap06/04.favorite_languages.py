38a876348c34ab9d4713acd350285654f4ef934b
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-09-24 11:15:38 -0300

    feat: add examples for dictionary usage and manipulation

---
+++ chap06/04.favorite_languages.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+    "sarah": "c",
+    "jen": "python",
+    "edward": "ruby",
+    "phil": "php",
+}
+
+print("Sarah's favorite language is " + favorite_languages["sarah"].title())
---
+++ chap06/exercise01_dict01.py
@@ -0,0 +1,12 @@
+person = {
+    "first_name": "John",
+    "last_name": "Connor",
+    "age": "33",
+    "city": "Nebraska"
+}
+
+print(person)
+print("First Name: " + person["first_name"])
+print("Last Name: " + person["last_name"])
+print("Age: " + person["age"])
+print("City: " + person["city"])
