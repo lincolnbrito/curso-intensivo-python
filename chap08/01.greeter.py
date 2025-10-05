3d210c1c4ad1b242eaeaee479777cf0eedec7b6b
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-10-05 17:24:09 -0300

    feat: function definition

---
+++ chap08/01.greeter.py
@@ -0,0 +1,5 @@
+def greet_user():
+    """Show a simple greeting message"""
+    print("Hello!")
+
+greet_user()
---
+++ chap08/02.greeter.py
@@ -0,0 +1,7 @@
+def greet_user(username):
+    """Shows a simple greeting message"""
+    print("Hello, " + username.title() + "!")
+
+greet_user("jesse")
+greet_user("james")
+greet_user("meow")
