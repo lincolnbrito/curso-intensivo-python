27f31d8c3441338625a3a817058953e059ea243c
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-10-06 17:15:38 -0300

    feat: import all functions from a module

---
+++ chap08/21.import_all_functions/making_pizzas.py
@@ -0,0 +1,13 @@
+from pizza import *
+"""
+Not recommended
+
+Use:
+    from pizza import make_pizza
+    from pizza import make_pizza as mp 
+or:
+    import pizza as p
+"""
+
+make_pizza(16, "pepperoni", "bacon")
+make_pizza(20, "green peppers", "mushrooms", "extra cheese")
