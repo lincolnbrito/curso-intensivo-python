0c2ddc6aa3624b2a09d45227cfbf66f5aa069661
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-10-05 22:34:41 -0300

    feat: exercise describe cities located in countries

---
+++ chap08/exercise05_cities.py
@@ -0,0 +1,7 @@
+def describe_city(city, country="Brazil"):
+    print(city + " is located in " + country)
+
+
+describe_city("Vitória da Conquista")
+describe_city("Salvador")
+describe_city("New York", "United States")
