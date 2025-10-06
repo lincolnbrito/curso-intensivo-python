0bb2327f91607011dd19011bffc4479952ddf050
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-10-06 16:56:32 -0300

    feat: import function with alias

---
+++ chap08/19.modules_import_alias_functions/making_pizzas.py
@@ -0,0 +1,4 @@
+from pizza import make_pizza as mp
+
+mp(16, "pepperoni", "bacon")
+mp(20, "green peppers", "mushrooms", "extra cheese")
---
+++ chap08/19.modules_import_alias_functions/pizza.py
@@ -0,0 +1,5 @@
+def make_pizza(size, *toppings):
+    """Present the pizza we are about to prepare"""    
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
