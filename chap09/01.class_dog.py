789288fcd97f096966761ac0c8391cec60c6be37
Lincoln Brito <lincoln.sbrito@gmail.com>
2025-10-07 21:50:44 -0300

    feat: first class example

---
+++ chap09/01.class_dog.py
@@ -0,0 +1,27 @@
+class Dog():
+    """An simple attempt to model a dog"""
+    def __init__(self, name, age):
+        """Initialize name and age attributes"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """Simulate a dog rolling over in response to a command"""
+        print(self.name.title() + " rolled over!")
+
+my_dog = Dog("willie", 6)
+your_dog = Dog("lessie", 3)
+
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
+my_dog.sit()
+my_dog.roll_over()
+
+print("\nYour dog's name is " + your_dog.name.title())
+print("Your dog is " + str(your_dog.age) + " years old.")
+your_dog.sit()
+your_dog.roll_over()
